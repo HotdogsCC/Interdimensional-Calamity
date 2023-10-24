e1e17e6e14ad73b4971b97d581103ec290851e99
HotdogsCC <chacha9669@hotmail.com>
2023-10-24 15:54:38 +1100

    sprinting + house fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private PlayerInput playerInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Moves the character
         rb.velocity = movement * Time.deltaTime * moveSpeed;
-        AnimateTheCharacter();
         
+        AnimateTheCharacter();
+
+        // Checks whether the sprint key is being held or nah
+        if (playerInput.actions["Sprint"].IsPressed())
+        {
+            moveSpeed = 150;
+        }
+        else
+        {
+            moveSpeed = 100;
+        }
     }
 
     private void AnimateTheCharacter()
@@ -87,153 +99,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnMovement(InputValue value)
     {
-        {
-            movement = value.Get<Vector2>();
-        }
+        movement = value.Get<Vector2>();
     }
-        //private void PlayerInput()
-        //{
-        //    var tempMoveSpeed = moveSpeed * Time.deltaTime;
-        //    var keysHeld = 0;
-        //    var up = false;
-        //    var down = false;
-        //    var left = false;
-        //    var right = false;
+        
+    private void OnSprint(InputValue value)
+    {
 
-        //    if (Input.GetKey(KeyCode.DownArrow))
-        //    {
-        //        keysHeld++;
-        //        down = true;
-        //        rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y - tempMoveSpeed, rb.transform.position.z);
-        //    }
-
-        //    if (Input.GetKey(KeyCode.LeftArrow))
-        //    {
-        //        left = true;
-        //        keysHeld++;
-        //        rb.transform.position = new Vector3(rb.transform.position.x - tempMoveSpeed, rb.transform.position.y, rb.transform.position.z);
-        //    }
-
-        //    if (Input.GetKey(KeyCode.RightArrow))
-        //    {
-        //        right = true;
-        //        keysHeld++;
-        //        rb.transform.position = new Vector3(rb.transform.position.x + tempMoveSpeed, rb.transform.position.y, rb.transform.position.z);
-        //    }
-
-        //    if (Input.GetKey(KeyCode.UpArrow))
-        //    {
-        //        up = true;
-        //        keysHeld++;
-        //        rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + tempMoveSpeed, rb.transform.position.z);
-        //    }
-
-
-        //    switch (keysHeld)
-        //    {
-        //        case 4:
-        //            animator.SetBool("movingDown", false);
-        //            animator.SetBool("movingUp", false);
-        //            animator.SetBool("movingLeft", false);
-        //            animator.SetBool("movingRight", false);
-        //            return;
-        //        case 3:
-        //            if (up == false)
-        //            {
-        //                animator.SetBool("movingDown", true);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            if (down == false)
-        //            {
-        //                animator.SetBool("movingUp", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            if (left == false)
-        //            {
-        //                animator.SetBool("movingRight", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //            }
-        //            if (right == false)
-        //            {
-        //                animator.SetBool("movingLeft", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            return;
-        //        case 2:
-        //            if(up && down)
-        //            {
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            else if (up)
-        //            {
-        //                animator.SetBool("movingUp", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            else if (down)
-        //            {
-        //                animator.SetBool("movingDown", true);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            else if (left && right)
-        //            {
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            return;
-        //        case 1:
-        //            if (up)
-        //            {
-        //                animator.SetBool("movingUp", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            if (down)
-        //            {
-        //                animator.SetBool("movingDown", true);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            if (left)
-        //            {
-        //                animator.SetBool("movingLeft", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingRight", false);
-        //            }
-        //            if (right)
-        //            {
-        //                animator.SetBool("movingRight", true);
-        //                animator.SetBool("movingDown", false);
-        //                animator.SetBool("movingUp", false);
-        //                animator.SetBool("movingLeft", false);
-        //            }
-        //            return;
-        //        case 0:
-        //            animator.SetBool("movingDown", false);
-        //            animator.SetBool("movingUp", false);
-        //            animator.SetBool("movingLeft", false);
-        //            animator.SetBool("movingRight", false);
-        //            return;
-        //    }
+    }
 
     
 }
